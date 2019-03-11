f40fbd093ad1b7b5254944127b217786fb63f1aa
Jerónimo Deiros <jeronimodeiros@gmail.com>
2019-03-10 23:55:25 -0300

    Vistas, plantillas, Archivos de inicio

---
+++ Controllers/LoanController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcPicashWeb.Models;
+using System;
+
+namespace MvcPicashWeb.Controllers
+{
+    public class LoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            var loan = new Loan
+            {
+                AñoFundacion = 2005,
+                LoanId = Guid.NewGuid().ToString(),
+                Nombre = "platzi school"
+            };
+
+            /*
+             * bolsa de cosas dinamicas, es lo mismo que ViewData["CosaDinamica"]
+             * son distintas formas de accedera a info extra que le mando desde aca
+             */
+            ViewBag.CosaDinamica = "La monja"; 
+
+            return View(loan);
+        }
+    }
+}
---
+++ Models/Loan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcPicashWeb.Models
+{
+    public class Loan
+    {
+        public string LoanId { get; set; }
+        public string Nombre { get; set; }
+        public int AñoFundacion { get; set; }
+    }
+}
