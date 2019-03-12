1ff553b765191269149c76a50a4293f2ea75cacb
Jerónimo Deiros <jeronimodeiros@gmail.com>
2019-03-11 22:22:11 -0300

    15 - Mostrando otras listas de objetos

--- Controllers/EscuelaController.cs
+++ Controllers/EscuelaController.cs
@@ -22,8 +22,9 @@ namespace MvcPicashWeb.Controllers
              * bolsa de cosas dinamicas, es lo mismo que ViewData["CosaDinamica"]
              * son distintas formas de accedera a info extra que le mando desde aca
              */
-            ViewBag.CosaDinamica = "La monja";
-            return View();
+            ViewBag.Date = DateTime.Now; //por ser dynamic le puedo asignar cualquier cosa, en este caso un datetime
+
+            return View(escuela);
         }
     }
 }
